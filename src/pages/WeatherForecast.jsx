4c048707b6e7b3d97d9e9e191b57b2ae5fedfe8d
Changhu Cheng <changhu.cheng@shopee.com>
2022-10-20 19:12:15 +0800

    feat: use effect

--- src/pages/WeatherForecast.jsx
+++ src/pages/WeatherForecast.jsx
@@ -7,53 +7,97 @@ import { get } from 'utils';
 // reverse geocoding: http://code.google.com/apis/maps/documentation/geocoding/#ReverseGeocoding
 
 export default function WeatherForecast(props) {
-  console.log('[WeatherForecast start rendering]');
+  console.log('\n[rendering] start');
 
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
   const [temperature, setTemperature] = useState(0); // option + shift + 8 => °
+  const [loadingFlag, setLoadingFlag] = useState(true);
+  const [timezoneId, setTimezoneId] = useState('GMT');
 
   useEffect(() => {
-    console.log('[WeatherForecast effect running]');
-
+    console.log('[effect] set latitude and longitude');
     navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
-      console.log(- (new Date()).getTimezoneOffset() / 60);
+      console.log('\t position: ', position);
+      // console.log(- (new Date()).getTimezoneOffset() / 60);
       setLatitude(position.coords.latitude);
       setLongitude(position.coords.longitude);
     });
+  }, []);
 
-    async function getWeatherForecast() {
-      const response = await get('https://api.open-meteo.com/v1/forecast', {
-        latitude,
-        longitude,
-        current_weather: true,
-        // http://api.geonames.org/timezoneJSON?lat=22.53&lng=113.94&username=tigercheng
-        timezone: 'Asia/Shanghai',
+  useEffect(() => {
+    console.log('[effect] fetchTimezoneId')
+    async function fetchTimezoneId() {
+      const response = await get('http://api.geonames.org/timezoneJSON', {
+        lat: latitude.toFixed(2),
+        lng: longitude.toFixed(2),
+        username: 'tigercheng',
       });
-      const weatherData = await response.json();
-      console.log(weatherData);
-      setTemperature(weatherData.current_weather.temperature);
+      const timezone = await response.json();
+      console.log('\t timezone', timezone);
+      setTimezoneId(timezone.timezoneId);
     }
 
-    async function getCityInfo() {
+    fetchTimezoneId();
+  }, [latitude, longitude]);
+
+  useEffect(() => {
+    console.log('[effect] fetchCityInfo');
+    async function fetchCityInfo() {
       const response = await get('http://api.geonames.org/countrySubdivisionJSON', {
         lat: latitude.toFixed(2),
         lng: longitude.toFixed(2),
         username: 'tigercheng',
       });
+      console.log('\t cityData', cityData);
       const cityData = await response.json();
-      console.log(cityData);
     }
 
-    getWeatherForecast();
-    getCityInfo();
+    fetchCityInfo();
   }, [latitude, longitude]);
+
+  useEffect(() => {
+    console.log('[effect] init data');
+
+    async function getWeatherForecast() {
+      try {
+        // console.log('\t timezoneId', timezoneId);
+        const response = await get('https://api.open-meteo.com/v1/forecast', {
+          latitude,
+          longitude,
+          current_weather: true,
+          // http://api.geonames.org/timezoneJSON?lat=22.53&lng=113.94&username=tigercheng
+          // timezone: 'Asia/Shanghai',
+          timezone: timezoneId,
+        });
+        const weatherData = await response.json();
+        console.log('\t weatherData', weatherData);
+        setTemperature(weatherData?.current_weather?.temperature);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    async function initData() {
+      // await fetchTimezoneId();
+      await getWeatherForecast();
+      setLoadingFlag(false);
+    }
+
+    initData();
+  }, [latitude, longitude, timezoneId]);
 
   return (
     <>
-      <div>weather forecast: Latitude(纬度): {latitude} Longitude(经度): {longitude}</div>
-      <div>{ temperature }°C</div>
+      {
+        !loadingFlag && (
+          <>
+            <div>weather forecast: Latitude(纬度): {latitude} Longitude(经度): {longitude}</div>
+            <div>{ temperature }°C</div>
+          </>
+        )
+      }
     </>
+    
   );
 };
